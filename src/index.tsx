f9d132e47d98ac36ae3d90036c5a1f231f22c40b
Jake <jakedves@gmail.com>
2022-04-02 14:41:51 +0100

    ReactionTime game ui done

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './stylesheets/index.css';
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import App from "./App";
+import ReactionTime from "./MiniGames/ReactionTime";
+import reportWebVitals from "./reportWebVitals";
+import "./stylesheets/index.css";
 
-
-const container = document.getElementById('root')
-if (!container) throw new Error("Missing root element")
-const root = ReactDOMClient.createRoot(container)
+const container = document.getElementById("root");
+if (!container) throw new Error("Missing root element");
+const root = ReactDOMClient.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-
+    <ReactionTime getTime={(num: number) => {}} />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// <ReactionTime getTime={(num: number) => {}} />     | Viewing views
