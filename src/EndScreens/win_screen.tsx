d04c65213c6898b84416df8b80fa0d8d1fae7382
Shivam Sethi <ss3618@bath.ac.uk>
2022-04-02 13:08:24 +0100

    Benaiah help

--- src/EndScreens/win_screen.tsx
+++ src/EndScreens/win_screen.tsx
@@ -1,5 +1,9 @@
+
 import React from 'react';
 import '../stylesheets/index.css';
+import firework from './Images/fireworks.png';
+
+
 
 // const container = document.getElementById('root')
 // if (!container) throw new Error("Missing root element")
@@ -9,9 +13,9 @@ const WinScreen = () => {
     const playerID = 2; // Temp winnerID
     return (
         <div className='flex flex-col items-center justify-center h-screen bg-green-400'>
-            <header className='text-4xl p-2 my-2 italic'>Congratulations Player {playerID}</header>
-            <header className='text-4xl p-2 my-2 font-bold'>You Win!</header>
-
+            {/* <header className='text-4xl p-2 my-2 italic'>Congratulations Player {playerID}</header>
+            <header className='text-4xl p-2 my-2 font-bold'>You Win!</header> */}
+            <img src={firework} alt="Cannot display" />
         </div>
     )
 }
