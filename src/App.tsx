8004485045ed5da556259c066dad023cde78e0c5
Wyn Price <wynprice999@gmail.com>
2022-04-03 01:11:13 +0100

    Hacky way of resetting the user when the socket closes

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,9 @@ import LoseScreen from './EndScreens/LoseScreen';
 
 const defaultState = "none"
 
+const locations = new URLSearchParams(window.location.search)
+window.history.pushState({}, document.title, window.location.pathname);
+const wasDisconncted = locations.get("disconnected") === "true"
 
 const connection = new Connection()
 
@@ -20,7 +23,6 @@ const App = () => {
   const [joinCode, setJoinCode] = useState("")
   const [connectionManager, setConnectionManager] = useState<ConnectionManagerPlayer>()
   const gameEngineRef = useRef<GameEngine>()
-
 
   const playerJoinedThis = () => {
     gameEngineRef.current = new GameEngine()
@@ -49,7 +51,6 @@ const App = () => {
     })
   }
 
-  console.log(state)
 
   if (state === "none") {
     return (
@@ -59,6 +60,9 @@ const App = () => {
           <div className='flex p-2 m-2 bg-red-500'>
             <input value={joinCode} onInput={e => setJoinCode(e.currentTarget.value)} placeholder='code' />
             <button onClick={joinRoom} className='ml-2'>Join</button>
+          </div>
+          <div>
+            {wasDisconncted ? "Error: Socket Disconnected" : ""}
           </div>
         </div>
       </div>
